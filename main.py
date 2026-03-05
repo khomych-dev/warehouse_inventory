5e6de8ebe35a9f494c00009b1602afdc801a03f1
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-05 19:27:12 +0200

    Removed comments

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 
-# Створюємо наш "двигун"
 app = FastAPI()
 
-# Головний ендпоінт (корінь сайту)
 @app.get("/")
 def read_root():
     return {"message": "Warehouse System Online, Bratishka"}
 
-# Ендпоінт для перевірки стану
 @app.get("/status")
 def get_status():
     return {"status": "Operational", "fuel_level": "100%"}
