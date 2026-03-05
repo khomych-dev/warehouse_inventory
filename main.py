1182c3b99c5d7ec2f7e6fae2da85e1cfcef41f96
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-05 19:50:24 +0200

    Added Pydantic model and endpoint for spare parts validation

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class SparePart(BaseModel):
+    name: str
+    price: float
+    quantity: int
+    category: str
 
 @app.get("/")
 def read_root():
@@ -14,3 +21,10 @@ def get_status():
 def get_tools():
     tools_list = ["Wrench", "Hammer", "Screwdriver", "Multimeter"]
     return {'tools': tools_list}
+
+@app.post("/add-part")
+def add_part(part: SparePart):
+    return {
+        "message": f"Item '{part.name}' successfully added!",
+        "received_data": part
+    }
