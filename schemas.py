35542396e9aca3380306306b7f468f8fee51f717
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-10 12:50:46 +0200

    Updated the schemas.py file

--- schemas.py
+++ schemas.py
@@ -2,9 +2,21 @@ from pydantic import BaseModel
 from typing import Optional
 
 
+class CategoryBase(BaseModel):
+    name: str
+
+class CategoryCreate(CategoryBase):
+    pass
+
+class CategorySchema(CategoryBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
 class SparePart(BaseModel):
     id: Optional[str] = None
     name: str
     price: float
     quantity: int
-    category: str
+    category_id: int
