797f14d9b73b59a3e210efe961547e2bd2f7e8d0
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-11 11:05:25 +0200

    Added class Manufacturer to the models.py file

--- models.py
+++ models.py
@@ -11,6 +11,14 @@ class Category(Base):
     
     parts = relationship('DBPart', back_populates='category')
     
+class Manufacturer(Base):
+    __tablename__ = 'manufacturers'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    
+    parts = relationship('DBPart', back_populates='manufacturer')
+    
 class DBPart(Base):
     __tablename__ = 'parts'
 
