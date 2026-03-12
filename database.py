6c7e3a833836bff887a86d228427f8248b390cbf
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-12 18:46:38 +0200

    Updated and added the set_sqlite_progma function

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./warehouse.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -16,3 +18,9 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+@event.listens_for(Engine, 'connect')
+def set_sqlite_progma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
